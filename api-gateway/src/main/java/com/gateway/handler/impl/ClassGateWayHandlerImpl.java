8192b0d7a86bbd3bbf4e84d72784f45edc3b8c7f
dungkt0801 <dungkt@youmed.vn>
2023-06-15 12:25:20 +0700

    create routes for classes service

--- api-gateway/src/main/java/com/gateway/handler/impl/ClassGateWayHandlerImpl.java
+++ api-gateway/src/main/java/com/gateway/handler/impl/ClassGateWayHandlerImpl.java
@@ -1,6 +1,11 @@
 package com.gateway.handler.impl;
 
+import static com.gateway.constants.Constants.CLASSES_BASE_PATH;
+import static com.gateway.constants.Constants.CLASSES_SERVICE_ROOT;
+
+import com.common.util.Util;
 import com.gateway.handler.ClassGatewayHandler;
+import com.gateway.util.ApiGatewayUtil;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.client.WebClient;
 import lombok.RequiredArgsConstructor;
@@ -11,19 +16,49 @@ public class ClassGateWayHandlerImpl implements ClassGatewayHandler {
   private final WebClient webClient;
 
   public void handleClasses(RoutingContext routingContext) {
-    // Code to handle GET request for classes
+
+    String query = routingContext.request().query();
+    String url = CLASSES_SERVICE_ROOT + CLASSES_BASE_PATH + routingContext.normalisedPath();
+    if(query != null) {
+      url += "?" + query;
+    }
+
+    webClient
+      .getAbs(url)
+      .send(result -> {
+        if(result.succeeded()) {
+          ApiGatewayUtil.onClientSuccessResponse(routingContext, result.result().statusCode(), result.result().bodyAsString());
+        } else {
+          Util.onErrorResponse(routingContext, 500, result.cause());
+        }
+      });
   }
 
   public void handleClassesPost(RoutingContext routingContext) {
-    // Code to handle POST request for classes
+    webClient
+      .postAbs(CLASSES_SERVICE_ROOT + CLASSES_BASE_PATH + routingContext.normalisedPath())
+      .sendJsonObject(routingContext.getBodyAsJson(), result -> {
+        if(result.succeeded()) {
+          ApiGatewayUtil.onClientSuccessResponse(routingContext, result.result().statusCode(), result.result().bodyAsString());
+        } else {
+          Util.onErrorResponse(routingContext, 500, result.cause());
+        }
+      });
   }
 
   public void handleClassesPut(RoutingContext routingContext) {
-    // Code to handle PUT request for classes
+    webClient
+      .putAbs(CLASSES_SERVICE_ROOT + CLASSES_BASE_PATH + routingContext.normalisedPath())
+      .sendJsonObject(routingContext.getBodyAsJson(), result -> {
+        if(result.succeeded()) {
+          ApiGatewayUtil.onClientSuccessResponse(routingContext, result.result().statusCode(), result.result().bodyAsString());
+        } else {
+          Util.onErrorResponse(routingContext, 500, result.cause());
+        }
+      });
   }
 
   public void handleClassesDelete(RoutingContext routingContext) {
-    // Code to handle DELETE request for classes
   }
 
 }
